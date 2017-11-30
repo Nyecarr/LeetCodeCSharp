6722ca3a12b29bb53c84c63f06801128ebd2a3c0
Nick <nyecarr@gmail.com>
2017-11-29 19:39:26 -0700

    Single Number implementation

---
+++ Problems/SingleNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeCSharp.Problems
+{
+  // Problem 136 - Single Number
+  // https://leetcode.com/problems/single-number/description/
+  public class SingleNumber
+  {
+    // Linear time, linear space - this could probably be improved with some math wizardry
+    public int GetNumber(int[] nums)
+    {
+      if (nums.Length == 1)
+        return nums.First();
+
+      var dict = new Dictionary<int, int>();
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+        if (!dict.ContainsKey(nums[i]))
+        {
+          dict.Add(nums[i], 1);
+        }
+        else
+        {
+          dict[nums[i]]++;
+        }
+      }
+
+      return dict.First(v => v.Value == 1).Key;
+    }
+
+    public int GetNumber_NLogNTime_ConstantSpace(int[] nums)
+    {
+      if (nums.Length == 1)
+        return nums.First();
+
+      nums = nums.OrderBy(n => n).ToArray();
+
+      for (int i = 0; i < nums.Length - 1; i += 2)
+      {
+        if (nums[i] != nums[i + 1])
+          return nums[i];
+      }
+
+      throw new InvalidOperationException("Each integer appears twice in the input array.");
+    }
+  }
+}
---
+++ Tests/SingleNumberTests.cs
@@ -0,0 +1,50 @@
+﻿using LeetCodeCSharp.Problems;
+using NUnit.Framework;
+using System;
+
+namespace LeetCodeCSharp.Tests
+{
+  [TestFixture]
+  public class SingleNumberTests
+  {
+    [Test]
+    public void SingleNumber_GetNumber_ReturnsExpectedNumber()
+    {
+      var nums = new[] { 1, 1, 2, 2, 3, 4, 4 };
+
+      var singleNumber = new SingleNumber();
+
+      Assert.That(singleNumber.GetNumber(nums), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void SingleNumber_GetNumberEdge_ReturnsExpectedNumber()
+    {
+      var nums = new[] { 2, 2, 1 };
+
+      var singleNumber = new SingleNumber();
+
+      Assert.That(singleNumber.GetNumber(nums), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void SingleNumber_OneNumberInList_ReturnsThatNumber()
+    {
+      var nums = new[] { 1 };
+
+      var singleNumber = new SingleNumber();
+
+      Assert.That(singleNumber.GetNumber(nums), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void SingleNumber_AllNumbersHaveAPair_InvalidOperationExceptionIsThrown()
+    {
+      var nums = new[] { 1, 1, 2, 2 };
+
+      var singleNumber = new SingleNumber();
+
+      Assert.That(() => singleNumber.GetNumber(nums), Throws.TypeOf<InvalidOperationException>());
+    }
+  }
+}
