f99ad66cc85fd15bf6faa9424b792a75582fc51a
Nick <nyecarr@gmail.com>
2017-11-29 17:27:44 -0700

    Add ReverseString implementation

---
+++ Problems/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace LeetCodeCSharp.Problems
+{
+  // Problem 344 - Reverse String
+  // https://leetcode.com/problems/reverse-string/description/
+  public class ReverseString
+  {
+    public string Reverse(string s)
+    {
+      var stringBuilder = new StringBuilder();
+
+      for(int i = s.Length - 1; i >= 0; i--)
+      {
+        stringBuilder.Append(s[i]);
+      }
+
+      return stringBuilder.ToString();
+    }
+  }
+}
---
+++ Tests/ReverseStringTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeCSharp.Problems;
+using NUnit.Framework;
+
+namespace LeetCodeCSharp.Tests
+{
+  [TestFixture]
+  public class ReverseStringTests
+  {
+    [TestCase("", "")]
+    [TestCase("hello", "olleh")]
+    [TestCase("helloWorld", "dlroWolleh")]
+    public void ReverseString_Reverse_SimpleTestsCases(string input, string expected)
+    {
+      var reverseString = new ReverseString();
+      Assert.That(reverseString.Reverse(input), Is.EqualTo(expected));
+    }
+  }
+}
