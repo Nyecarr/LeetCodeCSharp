7b3c7d9a32c82cbb794eb8b8e36dbc426cf91ed6
Nick <nyecarr@gmail.com>
2017-11-29 17:49:49 -0700

    FizzBuzz implementation
    The *dreaded* fizzbuzz

---
+++ Problems/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeCSharp.Problems
+{
+  public class FizzBuzz
+  {
+    // Problem 412 - Fizz Buzz
+    // https://leetcode.com/problems/fizz-buzz/description/
+    // 
+    // Could use a rules pattern if you worried about maintainability
+    public IList<string> Do(int n)
+    {
+      var results = new List<string>();
+
+      for (int i = 1; i <= n; i++)
+      {
+        var result = string.Empty;
+
+        if (i % 3 == 0)
+          result += "Fizz";
+
+        if (i % 5 == 0)
+          result += "Buzz";
+
+        if (string.IsNullOrEmpty(result))
+          result = i.ToString();
+
+        results.Add(result);
+      }
+
+      return results;
+    }
+  }
+}
---
+++ Tests/FizzBuzzTests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeCSharp.Problems;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCodeCSharp.Tests
+{
+  [TestFixture]
+  public class FizzBuzzTests
+  {
+    [Test]
+    public void FizzBuzz_15_ReturnsExpectedList()
+    {
+      var fizzBuzz = new FizzBuzz();
+
+      var results = fizzBuzz.Do(15);
+
+      for (int i = 0; i < 15; i++)
+      {
+        Assert.That(results[i] == expectedList[i], $"Expected {expectedList[i]} but was {results[i]}.");
+      }
+    }
+
+    private List<string> expectedList = new List<string>
+      {
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz"
+      };
+  }
+}
