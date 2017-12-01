d272445eb0ce9567592f6cf43c8a1b935103bf34
Nick <nyecarr@gmail.com>
2017-11-30 21:45:56 -0700

    Add MaximumDepth implementation

---
+++ Problems/MaximumDepth.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeCSharp.DataStructures;
+using System;
+
+namespace LeetCodeCSharp.Problems
+{
+  // Problem 104 - Maximum Depth of Binary Tree
+  // https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+  public class MaximumDepth
+  {
+    public int MaxDepth(TreeNode root)
+    {
+      if (root == null)
+        return 0;
+
+      return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+    }
+  }
+}
---
+++ Tests/MaximumDepthTests.cs
@@ -0,0 +1,40 @@
+﻿using LeetCodeCSharp.DataStructures;
+using LeetCodeCSharp.Problems;
+using NUnit.Framework;
+
+namespace LeetCodeCSharp.Tests
+{
+  [TestFixture]
+  public class MaximumDepthTests
+  {
+    [Test]
+    public void MaxDepth_NullRoot_ReturnsZero()
+    {
+      TreeNode root = null;
+
+      var maximumDepth = new MaximumDepth();
+
+      Assert.That(maximumDepth.MaxDepth(root), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void MaxDepth_OnlyRoot_ReturnsOne()
+    {
+      TreeNode root = new TreeNode(1);
+
+      var maximumDepth = new MaximumDepth();
+
+      Assert.That(maximumDepth.MaxDepth(root), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void MaxDepth_TreeWithDepthOfThree_ReturnsThree()
+    {
+      var root = new TreeNode(1, new TreeNode(2, new TreeNode(3, null, null), null), null);
+
+      var maximumDepth = new MaximumDepth();
+
+      Assert.That(maximumDepth.MaxDepth(root), Is.EqualTo(3));
+    }
+  }
+}
