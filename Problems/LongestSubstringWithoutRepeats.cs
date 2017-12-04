0a24f4748814a2d2b9bb3c8850424bea25ff9855
Nick <nyecarr@gmail.com>
2017-12-03 19:32:11 -0700

    Add LongestSubstringWithoutRepeatingCharacters implementation
    https://leetcode.com/problems/longest-substring-without-repeating-characters/description/

---
+++ Problems/LongestSubstringWithoutRepeats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Problems
+{
+  // Problem 3 - Longest Substring Without Repeating Characters
+  // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+  public class LongestSubstringWithoutRepeats
+  {
+    public int LengthOfLongestSubstring(string s)
+    {
+      // Sliding window implementation, O(n) time
+      var seenCharacters = new Dictionary<char, int>();
+      var maxLengthSoFar = 0;
+      var start = 0;
+
+      for (var end = 0; end < s.Length; end++)
+      {
+        if (seenCharacters.ContainsKey(s[end]))
+        {
+          start = Math.Max(start, seenCharacters[s[end]] + 1);
+          seenCharacters[s[end]] = end;
+        }
+        else
+        {
+          seenCharacters.Add(s[end], end);
+        }
+
+        maxLengthSoFar = Math.Max(maxLengthSoFar, end - start + 1);
+      }
+
+      return maxLengthSoFar;
+    }
+  }
+}
---
+++ Tests/LongestSubstringWithoutRepeatsTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeCSharp.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Tests
+{
+  [TestFixture]
+  public class LongestSubstringWithoutRepeatsTests
+  {
+    [TestCase("abcabcbb", 3)]
+    [TestCase("bbbbb", 1)]
+    [TestCase("pwwkew", 3)]
+    public void LengthOfLongestSubstring(string testString, int expectedLength)
+    {
+      var longestSubstring = new LongestSubstringWithoutRepeats();
+
+      Assert.That(longestSubstring.LengthOfLongestSubstring(testString), Is.EqualTo(expectedLength));
+    }
+  }
+}
